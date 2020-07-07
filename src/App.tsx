67dc0a2ddd54a1781fbe2c81b993eb35e11c49b0
Zerong Wang <zerongwang@WokeTotem.local>
2020-07-07 12:26:02 +1000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  makeStyles,
+  ThemeProvider,
+  Typography,
+  CssBaseline,
+} from "@material-ui/core";
+import React from "react";
+import bgURL from "./bg.png";
+import { theme } from "./theme";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#000403",
+    background: `url(${bgURL}) repeat`,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box className={classes.root} padding={3} minHeight="100vh">
+        <Typography color="primary" variant="h4">
+          GYG BURRITO
+        </Typography>
+      </Box>
+    </ThemeProvider>
   );
 }
 
